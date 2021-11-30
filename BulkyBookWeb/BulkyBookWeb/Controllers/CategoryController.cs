28bccf550463ef81f0e2a1a9e7b1c9819854a7c2
Rakuxt <andres91_07@hotmail.com>
2021-11-29 22:19:21 -0500

    Adding edit and delete actions

--- BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/BulkyBookWeb/Controllers/CategoryController.cs
@@ -29,7 +29,7 @@ namespace BulkyBookWeb.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category category)
         {
-            if(category.DisplayOrder.ToString() == category.Name)
+            if (category.DisplayOrder.ToString() == category.Name)
             {
                 ModelState.AddModelError("CustomErrors", "The DisplayOrder cannot exactly match the Name.");
             }
@@ -40,6 +40,74 @@ namespace BulkyBookWeb.Controllers
             }
 
             dbContext.Categories.Add(category);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFind = dbContext.Categories.Find(id);
+            //var categorySingle = dbContext.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (category.DisplayOrder.ToString() == category.Name)
+            {
+                ModelState.AddModelError("CustomErrors", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+
+            dbContext.Categories.Update(category);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = dbContext.Categories.FirstOrDefault(c => c.Id == id);
+            //var categoryFind = dbContext.Categories.Find(id);
+            //var categorySingle = dbContext.Categories.SingleOrDefault(c => c.Id == id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category category)
+        {
+            dbContext.Categories.Remove(category);
             dbContext.SaveChanges();
             return RedirectToAction("Index");
         }
